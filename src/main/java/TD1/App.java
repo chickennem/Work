ce61e1408c644983a6b87d62f1a22a21a3da9e4f
chickennem <xavsy@hotmail.com>
2018-10-02 18:45:28 +0200

    Add files via upload

---
+++ src/main/java/TD1/App.java
@@ -0,0 +1,13 @@
+package TD1;
+
+/**
+ * Hello world!
+ *
+ */
+import org.apache.log4j.Logger;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/TD1/LoggerMain.java
@@ -0,0 +1,13 @@
+package TD1;
+
+import org.apache.log4j.Logger;
+public class LoggerMain{
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args)
+    {
+        LOGGER.error("Hello World !");
+    }
+
+}
